5e7d90d454026f8be3fb04bdef39f0ea31ed83e5
RootSong <sgh8774@gmail.com>
2022-03-28 19:40:12 +0900

    Initial commit of Spring

---
+++ src/main/java/com/sejong/cultureuniverse/CultureUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.cultureuniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureUniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CultureUniverseApplication.class, args);
+    }
+
+}
