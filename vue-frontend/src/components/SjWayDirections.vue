d86a12f156f4a73d5356d520fc19bb8db1c81d9d
DESKTOP-TG40PLD\남경이놋북 <skaruddlek@gmail.com>
2022-03-30 21:33:53 +0900

    오시는길 FAQ MyPage Notice eventWinner EventInProgress EventInProgressConn EventClosed EventClosedConn

---
+++ vue-frontend/src/components/SjWayDirections.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <Header current-class="SjWayDirections"/>
+    <div class="container-fluid py-5">
+      <div class="container py-5">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 col-md-8 col text-center mb-4">
+            <h1 class="mb-4">세종문화회관 오시는 길 </h1>
+
+            <div class="tabmenu">
+              <ul>
+                <li id="tab1" class="btnCon"><input type="radio" checked name="tabmenu" id="tabmenu1">
+                  <label for="tabmenu1">자동차</label>
+                  <div class="tabCon"><img id="car" > 자동차 안내 이미지</div>
+                  <br><br><br><br><br>
+                </li>
+                <li id="tab2" class="btnCon"><input type="radio" name="tabmenu" id="tabmenu2">
+                  <label for="tabmenu2">대중교통</label>
+                  <div class="tabCon"><img id="transport" > 대중교통 안내 이미지</div>
+                </li>
+                <li id="tab3" class="btnCon"><input type="radio" name="tabmenu" id="tabmenu3">
+                  <label for="tabmenu3">도보</label>
+                  <div class="tabCon"><img id="walk" > 도보 안내 이미지</div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./header";
+import Footer from "./footer";
+
+export default {
+  name: "SjWayDirections",
+  components: {Header, Footer}
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-size: 15px;
+  line-height: 1.3;
+}
+
+ul {
+  list-style: none;
+}
+
+.tabmenu {
+  max-width: 600px;
+  margin: 0 auto;
+  position: relative;
+}
+
+.tabmenu ul li {
+  display: inline-block;
+  width: 33.33%;
+  float: left;
+  text-align: center;
+  background: #f9f9f9;
+  line-height: 40px;
+}
+
+.tabmenu label {
+  display: block;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+}
+
+.tabmenu input {
+  display: none;
+}
+
+.tabCon {
+  display: none;
+  text-align: left;
+  padding: 20px;
+  position: absolute;
+  left: 0;
+  top: 40px;
+  box-sizing: border-box;
+  border: 5px solid #f9f9f9;
+}
+
+.tabmenu input:checked ~ label {
+  background: #ccc;
+}
+
+.tabmenu input:checked ~ .tabCon {
+  display: block;
+}
+</style>
---
+++ vue-frontend/src/components/eventInProgressConn.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <Header current-class="EventInProgressConn"/>
+    <div id="centerColumn">
+      <h2>진행중인 이벤트</h2>
+      <div>
+        <hr>
+        <p>이벤트제목:</p>
+        <p>이벤트기간:</p>
+        <p>당첨자 발표:</p>
+        <hr>
+        <div id="container">
+          <h4>이벤트 내용 또는 이미지</h4>
+        </div>
+
+        <div class="comments-area">
+          <div class="contact-form blog-single-form">
+            <h3>댓글이벤트 참여</h3>
+            <form>
+              <div class="row">
+                <div class="col-sm-6 col-xs-12">
+                  <div class="form-group">
+                    <input type="text" class="form-control" id="firstname" placeholder="id" name="firstname">
+                  </div><!--/.form-group-->
+                </div><!--/.col-->
+              </div><!--/.row-->
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="form-group">
+                    <textarea class="form-control" rows="7" id="comment" placeholder="댓글입력"></textarea>
+                  </div><!--/.form-group-->
+                </div><!--/.col-->
+              </div><!--/.row-->
+              <div class="row">
+                <div class="col-sm-12">
+                  <div class="single-contact-btn">
+                    <button class="contact-btn" href="#" role="button">댓글 등록</button>
+                  </div><!--/.single-single-contact-btn-->
+                </div><!--/.col-->
+              </div><!--/.row-->
+            </form><!--/form-->
+
+          </div><!--/.contact-form-->
+          <ol class="comment-list">
+            <li class="comment">
+              <div class="comment-body xtra-comment-body">
+                <div class="comment-meta">
+                  <div class="comment-author">
+                    <img src="" alt="사람이미지"/>
+                  </div><!-- .comment-author -->
+                </div><!-- .comment-meta -->
+
+                <div class="comment-content">
+                  <b class="fn">
+                    <a href="#">
+                      ID입력되는곳
+                      <span>댓글등록일자</span>
+                    </a>
+                  </b>
+                  <p>
+                    당첨돼서 연극보고싶어요!
+                  </p>
+                </div><!-- .comment-content -->
+              </div><!-- .comment-body -->
+            </li>
+          </ol>
+        </div><!--/.comments-area-->
+      </div>
+    </div><!--centerColumn   -->
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./header";
+import Footer from "./footer";
+
+export default {
+  name: "eventInProgressConn",
+  components: {Header, Footer}
+}
+</script>
+<style scoped>
+
+</style>
