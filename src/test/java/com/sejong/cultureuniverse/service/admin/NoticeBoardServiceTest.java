b15881d0212b3baf1a6421186fc2b49a62836577
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-04-08 16:39:47 +0900

    불일치 테스트

--- src/test/java/com/sejong/cultureuniverse/service/admin/NoticeBoardServiceTest.java
+++ src/test/java/com/sejong/cultureuniverse/service/admin/NoticeBoardServiceTest.java
@@ -1,9 +1,8 @@
 package com.sejong.cultureuniverse.service.admin;
 
-import com.sejong.cultureuniverse.dto.admin.NoticeBoardAndAdminDTO;
-import com.sejong.cultureuniverse.dto.paging.PageRequestDTO;
+import com.sejong.cultureuniverse.dto.NoticeBoardAndAdminDTO;
+import com.sejong.cultureuniverse.dto.PageRequestDTO;
 import com.sejong.cultureuniverse.entity.admin.Admin;
-import com.sejong.cultureuniverse.service.admin.NoticeBoardService;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
