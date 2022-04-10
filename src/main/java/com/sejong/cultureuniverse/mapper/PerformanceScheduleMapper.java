da3fdfcf1a4670df29321408cb1f54d6b372236d
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-04-10 17:21:42 +0900

    MapStruct 라이브러리 테스트

--- src/main/java/com/sejong/cultureuniverse/mapper/PerformanceScheduleMapper.java
+++ src/main/java/com/sejong/cultureuniverse/mapper/PerformanceScheduleMapper.java
@@ -8,16 +8,18 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PerformanceScheduleMapper {
-    /*PerformanceScheduleMapper INSTANCE = Mappers.getMapper(PerformanceScheduleMapper.class);
+    PerformanceScheduleMapper INSTANCE = Mappers.getMapper(PerformanceScheduleMapper.class);
     
     @Mapping(target = "performanceDetails.performId", expression = "java(performanceScheduleDTO.getPerformId())")
-    @Mapping(target = "scheduleDate", expression = "java(performanceScheduleDTO.scheduleDate())")
-    @Mapping(target = "scheduleTime", expression = "java(performanceScheduleDTO.scheduleTime())")
+    @Mapping(target = "scheduleCode", expression = "java(performanceScheduleDTO.getScheduleCode())")
+    @Mapping(target = "scheduleDate", expression = "java(performanceScheduleDTO.getScheduleDate())")
+    @Mapping(target = "scheduleTime", expression = "java(performanceScheduleDTO.getScheduleTime())")
     PerformanceSchedule dtoToSchedule(PerformanceScheduleDTO performanceScheduleDTO);
     
     
     @Mapping(target = "performId", expression = "java(performanceSchedule.getPerformanceDetails().getPerformId())")
+    @Mapping(target = "scheduleCode", expression = "java(performanceSchedule.getScheduleCode())")
     @Mapping(target ="scheduleDate", expression = "java(performanceSchedule.getScheduleDate())")
     @Mapping(target ="scheduleTime", expression = "java(performanceSchedule.getScheduleTime())")
-    PerformanceScheduleDTO scheduleToDto(PerformanceSchedule performanceSchedule);*/
+    PerformanceScheduleDTO scheduleToDto(PerformanceSchedule performanceSchedule);
 }
