aeef9a45e991da2a42d4f428668f953d2c3cdef3
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-04-13 18:16:38 +0900

    요일 처리 및 남은 좌석 표시, 일부 파일 경로 변경

--- src/main/java/com/sejong/cultureuniverse/service/reservation/ScheduleService.java
+++ src/main/java/com/sejong/cultureuniverse/service/reservation/ScheduleService.java
@@ -2,19 +2,14 @@ package com.sejong.cultureuniverse.service.reservation;
 
 import com.sejong.cultureuniverse.dto.paging.PageRequestDTO;
 import com.sejong.cultureuniverse.dto.paging.PageResultDTO;
-import com.sejong.cultureuniverse.dto.performances.PerformanceListDTO;
 import com.sejong.cultureuniverse.dto.performances.PerformanceScheduleDTO;
-import com.sejong.cultureuniverse.entity.performance.PerformanceDetails;
 import com.sejong.cultureuniverse.entity.performance.PerformanceSchedule;
 import com.sejong.cultureuniverse.mapper.PerformanceScheduleMapper;
 import com.sejong.cultureuniverse.repository.performances.PerformanceScheduleRepository;
-import java.util.List;
+import com.sejong.cultureuniverse.repository.reservation.SeatsRepository;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScheduleService {
     
     private final PerformanceScheduleRepository scheduleRepository;
+    private final SeatsRepository seatsRepository;
     
     public PageResultDTO<PerformanceScheduleDTO, PerformanceSchedule> getScheduleList(
         String performanceCode, PageRequestDTO pageRequestDTO) {
@@ -32,7 +28,9 @@ public class ScheduleService {
         Page<PerformanceSchedule> scheduleList = scheduleRepository.findByPerformanceDetails_PerformCode(
             performanceCode, pageRequestDTO.getPageable(Sort.by("scheduleCode").ascending()));
         
-        Function<PerformanceSchedule, PerformanceScheduleDTO> fn = (PerformanceScheduleMapper.INSTANCE::scheduleToDto);
+        
+        Function<PerformanceSchedule, PerformanceScheduleDTO> fn = (en ->
+            PerformanceScheduleMapper.INSTANCE.scheduleToDto(en, seatsRepository));
         
         return new PageResultDTO<>(scheduleList, fn);
     }
