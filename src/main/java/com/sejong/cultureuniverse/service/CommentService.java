260fef88ab2bcdca323aed028e5866277a576c34
donghoon <hoon_25@nate.com>
2022-04-16 17:05:42 +0900

    4.16

--- src/main/java/com/sejong/cultureuniverse/service/CommentService.java
+++ src/main/java/com/sejong/cultureuniverse/service/CommentService.java
@@ -1,27 +1,21 @@
 package com.sejong.cultureuniverse.service;
 
-import com.sejong.cultureuniverse.dto.CommentRequestDto;
-import com.sejong.cultureuniverse.entity.Member;
-import com.sejong.cultureuniverse.entity.event.EventBoard;
-import com.sejong.cultureuniverse.entity.event.EventComment;
-import com.sejong.cultureuniverse.repository.CommentRepository;
 import com.sejong.cultureuniverse.repository.EventBoardRepository;
 import com.sejong.cultureuniverse.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CommentService {
-    private final CommentRepository commentRepository;
+    //private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
     private final EventBoardRepository eventBoardRepository;
 
     //create
-    @Transactional
+    /*@Transactional
     public Long commentSave(String username, Long eventIdx, CommentRequestDto dto){
-        Member member = memberRepository.findByNickname(username);
+        //Member member = memberRepository.findByNickname(username);
         EventBoard eventBoard = eventBoardRepository.findById(eventIdx).orElseThrow(() ->
             new IllegalArgumentException("댓글 쓰기 실패: 해당글이 존재하지 않습니다."+eventIdx));
         dto.setMember(member);
@@ -31,6 +25,6 @@ public class CommentService {
         commentRepository.save(comment);
 
         return dto.getId();
-    }
+    }*/
 
 }
